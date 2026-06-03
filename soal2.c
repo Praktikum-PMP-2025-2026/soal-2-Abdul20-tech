da1e9c4829b3cbd1d5e6d7595697c5f86b8ac01c
Abdul20-tech <syakuroabdullah@gmail.com>
2026-06-03 08:28:55 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,5 +1,35 @@
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2025/2026
+ *   Modul               : 01 - Overview of C Language
+ *   Hari dan Tanggal    : Rabu, 3 Juni 2026
+ *   Nama (NIM)          : Abdullah (13224101)
+ *   Nama File           : Prak 1.c
+ *   Deskripsi           : Program menerima input jumlah bilangan yang di-input dan memberikan
+ *                         output jumlah iterasi yang diperlukan hingga nilai menjadi 1.
+ * 
+ */
+
+#include <string.h>
 #include <stdio.h>
 
-int main(void) {
-    return 0;
+int main(){
+    int N, data[256];
+
+    scanf("%d", &N);
+    for(int i=0; i<N; i++){
+        scanf("%d", &data[i]);
+    }
+
+    for (int i = 0; i < N; i++){
+        int count=0;
+        while (data[i] != 1){
+            if (data[i] % 2 == 0){
+                data[i] = data[i]/2;
+            }else{
+                data[i] = (3*data[i])+1;
+            }
+            count++;
+        }
+        printf("LANGKAH %d\n", count);
+    }
 }
+
